335d4dae6d8c98851a2a3405555671f085b5c683
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-28 11:48:36 -0300

    Created sign-in and sign-up components of home app

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
+    redirectTo: 'home/sign-in',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    redirectTo: 'home/sign-in',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'sign-in',
+        component: SignInFormComponent
+      }
+    ]
   }
 ];
 
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { SignInFormComponent } from './components/sign-in-form/sign-in-form.component';
+import { SignUpFormComponent } from './components/sign-up-form/sign-up-form.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SignInFormComponent, SignUpFormComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
