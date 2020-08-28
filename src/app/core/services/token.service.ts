74229d8f8b51780bedf3ad77265c7615cb0e6062
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-28 12:43:45 -0300

    Created token service

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import * as jwt_decode from 'jwt-decode';
+
+
+const KEYTOKEN = 'authToken';
+const KEYTOKENREFRESH = 'refreshToken';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+
+}
