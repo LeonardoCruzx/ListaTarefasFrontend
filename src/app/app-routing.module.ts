c847ebf24e4d0832a9fd97b5c57f00d83ffa8c42
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-28 13:27:57 -0300

    Created auth service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/guard/auth.guard'; 
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   },
+  {
+    path: 'user',
+    loadChildren: () => import('./user-logged/user-logged.module').then(m => m.UserLoggedModule),
+    canActivate: [AuthGuard]
+  }
 ];
 
 @NgModule({
