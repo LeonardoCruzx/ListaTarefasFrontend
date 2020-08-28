4464be9b26a9d793fa2009baff3100e0a5fe258c
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-28 13:31:05 -0300

    Improved auth service

--- src/app/home/services/auth.service.ts
+++ src/app/home/services/auth.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
+
+const AUTH_URL = 'http://192.168.0.106:8000/user/auth/';
+const REFRESH_URL = 'http://192.168.0.106:8000/user/auth/refresh/';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    ){}
+
+  authenticate(email: string, password: string){
+    email = email.toLowerCase();
+    return this.http.post(AUTH_URL,{email,password});
+  }
+
+  refresh(token:string){
+    return this.http.post(REFRESH_URL,{refresh:token})
+  }
 }
