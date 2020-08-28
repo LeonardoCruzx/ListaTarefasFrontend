f71c0010e32fa44eeb072d30f023ceb28a51e860
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-28 13:05:15 -0300

    Improved token service

--- src/app/core/services/token.service.ts
+++ src/app/core/services/token.service.ts
@@ -10,5 +10,46 @@ const KEYTOKENREFRESH = 'refreshToken';
 })
 export class TokenService {
 
+  authResult(authResult){
+    window.localStorage.setItem(KEYTOKENREFRESH,JSON.stringify(authResult.refresh));
+    window.localStorage.setItem(KEYTOKEN,JSON.stringify(authResult.access));
+    const d = new Date(0);
+    d.setUTCSeconds(this.decodePayloadJWT().exp)
+    window.localStorage.setItem("expires_at",JSON.stringify(d));
+  }
+  setToken(data){
+    window.localStorage.setItem(KEYTOKEN,JSON.stringify(data.access));
+    const d = new Date(0);
+    d.setUTCSeconds(this.decodePayloadJWT().exp)
+    window.localStorage.setItem("expires_at",JSON.stringify(d));
+  }
 
+  getToken(){
+    return JSON.parse(window.localStorage.getItem(KEYTOKEN));
+  }
+
+  getRefreshToken(){
+    return JSON.parse(window.localStorage.getItem(KEYTOKENREFRESH));
+  }
+
+  getExpiration(){
+    return JSON.parse(localStorage.getItem("expires_at"));
+  }
+
+  logout(){
+    window.localStorage.removeItem(KEYTOKEN);
+    window.localStorage.removeItem(KEYTOKENREFRESH);
+  }
+
+  isLogged(){
+    return !!window.localStorage.getItem(KEYTOKEN);
+  }
+
+  decodePayloadJWT(): any {
+    try {
+      return jwt_decode(this.getToken());
+    } catch (Error) {
+      return null;
+    }
+  }
 }
