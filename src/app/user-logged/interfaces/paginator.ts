6ea7297e75b7a2ff5dd7ea36d141e7cb838f57d5
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-29 18:34:34 -0300

    Improved paginator interface

--- src/app/user-logged/interfaces/paginator.ts
+++ src/app/user-logged/interfaces/paginator.ts
@@ -1,2 +1,12 @@
-export interface Paginator{
+export interface Paginator<T>{
+    links: {
+        previous: String;
+        next: String;
+    };
+    total_results: Number;
+    total_pages: Number;
+    current_page: Number;
+    page_size: Number;
+    results: T[]
+
 }
