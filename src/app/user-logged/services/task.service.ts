1b711415af201a2b893b75dac3102077e407b8a2
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-29 10:09:49 -0300

    Created get tasks method in task service

--- src/app/user-logged/services/task.service.ts
+++ src/app/user-logged/services/task.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const LIST_URL = 'http://127.0.0.1:8000/task/list/';
+const DETAIL_URL = 'http://127.0.0.1:8000/task/detail/';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  getTasks(): Observable<any>{
+    return this.http.get(LIST_URL);
+  }
 }
