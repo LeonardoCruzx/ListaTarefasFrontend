5257f7411a190f53d245544dbb0f30f5fd26850a
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-29 18:17:07 -0300

    Improved task interface

--- src/app/user-logged/interfaces/task.ts
+++ src/app/user-logged/interfaces/task.ts
@@ -1,2 +1,10 @@
 export interface Task {
+    id: Number;
+    created_at: Date;
+    updated_at: Date;
+    final_date?: Date;
+    content: String;
+    concluded: Boolean;
+    user: Number;
+    category: Number;
 }
