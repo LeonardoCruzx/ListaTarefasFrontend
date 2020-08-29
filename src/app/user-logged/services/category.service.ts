eaf0ea90e554a8daafcdd3614ad698cd20d7231a
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-29 18:39:53 -0300

    Create get categories method in category service

--- src/app/user-logged/services/category.service.ts
+++ src/app/user-logged/services/category.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Paginator } from '../interfaces/paginator';
+import { Category } from '../interfaces/category';
+
+const LIST_URL = 'http://127.0.0.1:8000/core/list/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCategories(): Observable<Paginator<Category>>{
+    return this.http.get<Paginator<Category>>(LIST_URL);
+  }
 }
