fcc4e2d68eaeb3b2c8237756b686284f84055ea2
leonardo <leonardo.m.cruz@hotmail.com>
2020-08-29 17:52:26 -0300

    Create get user method in user service

--- src/app/user-logged/services/user.service.ts
+++ src/app/user-logged/services/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
+import { TokenService } from 'src/app/core/services/token.service';
+
+const DETAIL_URL = 'http://127.0.0.1:8000/user/detail/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private tokenService: TokenService
+  ) { }
+
+  getUser(): Observable<User>{
+    return this.http.get<User>(DETAIL_URL+ this.tokenService.decodePayloadJWT().id + "/");
+  }
 }
